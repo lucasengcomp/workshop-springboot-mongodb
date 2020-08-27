ff132e94b952df0f6ec67ccb0be1ac1775c2c87d
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-08-27 20:55:22 -0300

    Criando services e organizando arquitetura da api

--- api/src/main/java/com/api/workshopmongo/resources/UserResource.java
+++ api/src/main/java/com/api/workshopmongo/resources/UserResource.java
@@ -1,27 +1,26 @@
 package com.api.workshopmongo.resources;
 
 import com.api.workshopmongo.domain.User;
+import com.api.workshopmongo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria", "maria@email.com");
-        User lucas = new User("2", "Lucas", "lucas@email.com");
-        List<User> list = new ArrayList<>();
 
-        list.addAll(Arrays.asList(maria, lucas));
-
-        return ResponseEntity.ok(list);
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
 }
---
+++ api/src/main/java/com/api/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.api.workshopmongo.services;
+
+import com.api.workshopmongo.domain.User;
+import com.api.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
