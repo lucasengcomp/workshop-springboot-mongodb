49dab68f4b06166d854350f41734c2663e7e15e2
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-08-29 20:52:36 -0300

    Implementando ObjectNotFoundException e busca de usuário pelo id

--- api/src/main/java/com/api/workshopmongo/services/UserService.java
+++ api/src/main/java/com/api/workshopmongo/services/UserService.java
@@ -2,6 +2,7 @@ package com.api.workshopmongo.services;
 
 import com.api.workshopmongo.domain.User;
 import com.api.workshopmongo.repository.UserRepository;
+import com.api.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,13 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        User user = repository.findOne(id);
+        if (user == null) {
+            throw new ObjectNotFoundException("Usuário não encontrado!");
+        }
+        return user;
     }
 }
