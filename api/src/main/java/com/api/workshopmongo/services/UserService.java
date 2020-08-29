0919071497878e07b0cb8590f52dc5f9f305d6d8
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-08-29 20:55:26 -0300

    Correção findById User por id

--- api/src/main/java/com/api/workshopmongo/services/UserService.java
+++ api/src/main/java/com/api/workshopmongo/services/UserService.java
@@ -1,12 +1,13 @@
 package com.api.workshopmongo.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.api.workshopmongo.domain.User;
 import com.api.workshopmongo.repository.UserRepository;
 import com.api.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class UserService {
@@ -19,10 +20,8 @@ public class UserService {
     }
 
     public User findById(String id) {
-        User user = repository.findOne(id);
-        if (user == null) {
-            throw new ObjectNotFoundException("Usuário não encontrado!");
-        }
-        return user;
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
 }
