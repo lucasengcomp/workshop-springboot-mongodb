e01c32dfc12a19cbfbe85f861f3162133d8085b9
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-08-30 18:25:14 -0300

    Cadastrando um User

--- api/src/main/java/com/api/workshopmongo/services/UserService.java
+++ api/src/main/java/com/api/workshopmongo/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import com.api.workshopmongo.domain.User;
+import com.api.workshopmongo.dto.UserDTO;
 import com.api.workshopmongo.repository.UserRepository;
 import com.api.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,4 +25,11 @@ public class UserService {
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
+    public User inser(User obj) {
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+    }
 }
