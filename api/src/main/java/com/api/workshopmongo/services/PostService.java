cff4f55a14cad4343bf95a154a8b6b92dd36b115
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2020-08-30 23:53:44 -0300

    Consulta com @Query fullsearch

--- api/src/main/java/com/api/workshopmongo/services/PostService.java
+++ api/src/main/java/com/api/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.api.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,11 @@ public class PostService {
 //    }
 
     public List<Post> searchTitle(String text) {
-        return repository.findByTitleContainingIgnoreCase(text);
+        return repository.searchTitile(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 100);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
